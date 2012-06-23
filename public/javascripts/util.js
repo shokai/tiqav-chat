6572a90af8b9f0bd8d239773bd0fc54b10d468b4
Sho Hashimoto <hashimoto@shokai.org>
2012-06-24 00:18:09 +0900

    embed image URL

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,7 +1,11 @@
 String.prototype.markup = function(){
-    var s = this.escape_html();
-    s = s.replace(/(https?\:\/\/[^\s]+)/g, '<a href="$1">$1</a>');
-    return s;
+    return this.escape_html().split(/(\s+)/).map(function(i){
+        if(i.match(/^\s+$/)) return i;
+        if(i.match(/^https?\:\/\/[^\s]+\.(jpe?g|gif|png)$/)){
+            return i.replace(/^(https?\:\/\/[^\s]+)\.(jpe?g|gif|png)$/g, '<img src="$1.$2">');
+        }
+        return i.replace(/^(https?\:\/\/[^\s]+)$/g, '<a href="$1">$1</a>');
+    }).join('');
 };
 
 String.prototype.escape_html = function(){
