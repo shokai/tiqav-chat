3211d7804a415672822f9b445df83efa4a64af22
Sho Hashimoto <hashimoto@shokai.org>
2012-06-23 22:33:21 +0900

    display twitter icon from username

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,20 @@
 
 var socket = io.connect();
 
+var create_chat_html = function(msg){
+    var chat = $('<div>');
+
+    if(msg.user.match(/^[a-zA-Z0-9_]+$/)){
+        chat.append( $('<span>').addClass('icon').html(
+            $('<img>').attr('src', 'http://twiticon.herokuapp.com/'+msg.user)
+        ) );
+    }
+    chat.append( $('<span>').addClass('user').text(msg.user) );
+    chat.append(' : ');
+    chat.append( $('<span>').addClass('body').text(msg.body) );
+    return chat;
+};
+
 $(function(){
     $('#btn_send').click(function(e){
         var msg = {body: $('#body').val(), user: $('#user').val()};
@@ -8,14 +22,14 @@ $(function(){
     });
 
     socket.on('posted', function(data){
-        var li = $('<li>').text(data.message.user + ' : ' + data.message.body);
+        var li = $('<li>').html(create_chat_html(data.message));
         $('#chat').prepend(li);
     });
 
     socket.on('connected', function(data){
         for(var i = 0; i < data.message.length; i++){
-            var c = data.message[i];
-            var li = $('<li>').text(c.user + ' : ' + c.body);
+            var msg = data.message[i];
+            var li = $('<li>').html(create_chat_html(msg));
             $('#chat').prepend(li);
         }
     });
