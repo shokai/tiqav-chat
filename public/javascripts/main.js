1db77457c7b8b9c2b9a1d0f08ace397eb29dddc9
Sho Hashimoto <hashimoto@shokai.org>
2012-09-27 13:33:01 +0900

    fix indent javascript

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,90 +2,90 @@
 var socket = io.connect();
 
 var create_chat_html = function(msg){
-    var chat = $('<div>');
-    if(msg.user.match(/^[a-zA-Z0-9_]+$/)){
-        chat.append( $('<span>').addClass('icon').html(
-            $('<img>').attr('src', 'http://twiticon.herokuapp.com/'+msg.user)
-        ) );
-    }
-    chat.append( $('<span>').addClass('user').text(msg.user) );
-    chat.append(' : ');
-    chat.append( $('<span>').addClass('body').html(msg.body.markup()) );
-    return chat;
+  var chat = $('<div>');
+  if(msg.user.match(/^[a-zA-Z0-9_]+$/)){
+    chat.append( $('<span>').addClass('icon').html(
+      $('<img>').attr('src', 'http://twiticon.herokuapp.com/'+msg.user)
+    ) );
+  }
+  chat.append( $('<span>').addClass('user').text(msg.user) );
+  chat.append(' : ');
+  chat.append( $('<span>').addClass('body').html(msg.body.markup()) );
+  return chat;
 };
 
 var Tiqav = new function(){
-    var self = this;
+  var self = this;
 
-    this.cache = {};
+  this.cache = {};
 
-    this.search = function(word){
-        if(word === null || word === ''){
-            $('#tiqav').html('');
-            return;
-        }
-        if(self.cache[word] === undefined){
-            $.getJSON("http://api.tiqav.com/search.json?q="+word+"&callback=?", function(res){
-                self.cache[word] = res;
-                self.on_search(res);
-            });
-        }
-        else{
-            self.on_search(self.cache[word]);
-        }
-    };
+  this.search = function(word){
+    if(word === null || word === ''){
+      $('#tiqav').html('');
+      return;
+    }
+    if(self.cache[word] === undefined){
+      $.getJSON("http://api.tiqav.com/search.json?q="+word+"&callback=?", function(res){
+        self.cache[word] = res;
+        self.on_search(res);
+      });
+    }
+    else{
+      self.on_search(self.cache[word]);
+    }
+  };
 
-    this.on_search = function(data){
-        $('#tiqav').html('');
-        var max_images = data.length > 6 ? 6 : data.length;
-        for(var index = 0; index <  max_images; index++){
-            (function(){
-                var i = data[index];
-                var img_url = 'http://img.tiqav.com/'+i.id+'.th.'+i.ext;
-                var img = $('<img>').
-                    attr('src', img_url).
-                    click(function(e){
-                        $('#body').val(img_url).focus();
-                        self.search(null);
-                    });
-                $('#tiqav').prepend($('<li>').html(img));
-            })();
-        }
-    };
+  this.on_search = function(data){
+    $('#tiqav').html('');
+    var max_images = data.length > 6 ? 6 : data.length;
+    for(var index = 0; index <  max_images; index++){
+      (function(){
+        var i = data[index];
+        var img_url = 'http://img.tiqav.com/'+i.id+'.th.'+i.ext;
+        var img = $('<img>').
+          attr('src', img_url).
+          click(function(e){
+            $('#body').val(img_url).focus();
+            self.search(null);
+          });
+        $('#tiqav').prepend($('<li>').html(img));
+      })();
+    }
+  };
 }();
 
 var chat_post = function(e){
-    var msg = {body: $('#body').val(), user: $('#user').val()};
-    socket.emit('post', msg);
-    $('#body').val('');
+  var msg = {body: $('#body').val(), user: $('#user').val()};
+  socket.emit('post', msg);
+  $('#body').val('');
 };
 
 $(function(){
-    var sid = null;
-    $('#body').keyup(function(e){
-        clearTimeout(sid);
-        sid = setTimeout(function(){
-            Tiqav.search($('#body').val());
-        }, 300);
-    });
+  var sid = null;
+  $('#body').keyup(function(e){
+    clearTimeout(sid);
+    sid = setTimeout(function(){
+      Tiqav.search($('#body').val());
+    }, 300);
+  });
 
-    $('#body').keydown(function(e){
-        if(e.keyCode === 13){
-            chat_post();
-        }
-    });
-    $('#btn_send').click(chat_post);
+  $('#body').keydown(function(e){
+    if(e.keyCode === 13){
+      chat_post();
+    }
+  });
+  $('#btn_send').click(chat_post);
 
-    socket.on('posted', function(data){
-        var li = $('<li>').html(create_chat_html(data.message));
-        $('#chat').prepend(li);
-    });
+  socket.on('posted', function(data){
+    var li = $('<li>').html(create_chat_html(data.message));
+    $('#chat').prepend(li);
+  });
 
-    socket.on('connected', function(data){
-        for(var i = 0; i < data.message.length; i++){
-            var msg = data.message[i];
-            var li = $('<li>').html(create_chat_html(msg));
-            $('#chat').prepend(li);
-        }
-    });
+  socket.on('connected', function(data){
+    for(var i = 0; i < data.message.length; i++){
+      var msg = data.message[i];
+      var li = $('<li>').html(create_chat_html(msg));
+      $('#chat').prepend(li);
+    }
+  });
 });
--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,39 +1,39 @@
 String.prototype.markup = function(){
-    return this.escape_html().split(/(\s+)/).map(function(i){
-        if(i.match(/^\s+$/)) return i;
-        if(i.match(/^https?\:\/\/[^\s]+\.(jpe?g|gif|png)$/)){
-            return i.replace(/^(https?\:\/\/[^\s]+)\.(jpe?g|gif|png)$/g, '<img src="$1.$2">');
-        }
-        return i.replace(/^(https?\:\/\/[^\s]+)$/g, '<a href="$1">$1</a>');
-    }).join('');
+  return this.escape_html().split(/(\s+)/).map(function(i){
+    if(i.match(/^\s+$/)) return i;
+    if(i.match(/^https?\:\/\/[^\s]+\.(jpe?g|gif|png)$/)){
+      return i.replace(/^(https?\:\/\/[^\s]+)\.(jpe?g|gif|png)$/g, '<img src="$1.$2">');
+    }
+    return i.replace(/^(https?\:\/\/[^\s]+)$/g, '<a href="$1">$1</a>');
+  }).join('');
 };
 
 String.prototype.escape_html = function(){
-    var span = document.createElement('span');
-    var txt =  document.createTextNode('');
-    span.appendChild(txt);
-    txt.data = this;
-    return span.innerHTML;
+  var span = document.createElement('span');
+  var txt =  document.createTextNode('');
+  span.appendChild(txt);
+  txt.data = this;
+  return span.innerHTML;
 };
 
 var Notifer = new function(){
-    var self = this;
+  var self = this;
 
-    this.request = function(){
-        if(window.webkitNotifications.checkPermission() == 1){
-            window.webkitNotifications.requestPermission();
-        }
-    };
+  this.request = function(){
+    if(window.webkitNotifications.checkPermission() == 1){
+      window.webkitNotifications.requestPermission();
+    }
+  };
 
-    this.notify = function(icon, title, body){
-        if(window.webkitNotifications.checkPermission() == 0){
-            var notif = window.webkitNotifications.createNotification(icon, title, body);
-            notif.ondisplay = function(){
-                setTimeout(function(){
-                    if(notif.cancel) notif.cancel();
-                }, 3000);
-            };
-            notif.show();
-        }
-    };
+  this.notify = function(icon, title, body){
+    if(window.webkitNotifications.checkPermission() == 0){
+      var notif = window.webkitNotifications.createNotification(icon, title, body);
+      notif.ondisplay = function(){
+        setTimeout(function(){
+          if(notif.cancel) notif.cancel();
+        }, 3000);
+      };
+      notif.show();
+    }
+  };
 }();
